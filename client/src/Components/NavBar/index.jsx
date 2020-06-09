93c396b7931538eb1d353af4a1b8b78c0a36b3aa
michaeljgaeta <michaeljgaeta@gmail.com>
2020-06-09 12:06:47 +0100

    navbar form

--- client/src/Components/NavBar/index.jsx
+++ client/src/Components/NavBar/index.jsx
@@ -1,12 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Constructor method ran");
+    this.state = {
+      query: ""
+    };
+  }
+
+  handleFormSubmission = (event) => {
+    event.preventDefault();
+    const { query } = this.state;
+    alert(`Query: ${query}`);
+  };
+
+  handleInputChange = (event) => {
+    console.log(event.target.name);
+    console.log(event.target.value);
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h2>Navbar</h2>
+      <div className="App">
+        <form onSubmit={this.handleFormSubmission}>
+          <input
+            name="query"
+            id="search-input"
+            type="text"
+            placeholder="Search for a city..."
+            value={this.state.query} //query passed into search bar
+            onChange={this.handleInputChange}
+            autoComplete="on"
+          />
+          <button>🔎</button>
+        </form>
       </div>
-    )
+    );
   }
 }
 
